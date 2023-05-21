aedb4e3e76cd583189c25b40388cf5dac82fa924
AndreyDodul <andrey.dodul@mail.ru>
2023-05-21 22:49:52 +0300

    To find wished number

---
+++ Task50d_WishedNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, и
+// возвращает индексы числа в двумерном массиве или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4              // 17 -> такого числа в массиве нет
+
+// Вариант 1
+int m = 3, n = 4;
+int wishedNumber = 5; //Искомое число
+int[,] array = new int[m, n]; //Массив
+
+bool f = false;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        if (wishedNumber == array[i, j]) { m = i; n = j; f = true; };
+        //Console.Write("{0}\t", arr[i, j]);  // увеличивает пробелы между числами при выводе
+        Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+if (f) {
+    Console.WriteLine("Число " + wishedNumber + " найдено. Строка " + (m + 1) + " Столбец " + (n + 1));
+}
+else {
+    Console.WriteLine("Число " + wishedNumber + " в массиве не найдено");
+}
--- Task50d_WishedNumber/obj/Debug/net6.0/Task50d_WishedNumber.AssemblyInfo.cs
+++ Task50d_WishedNumber/obj/Debug/net6.0/Task50d_WishedNumber.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task52d_AverColumn")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task50d_WishedNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task52d_AverColumn")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task52d_AverColumn")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task50d_WishedNumber")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task50d_WishedNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
