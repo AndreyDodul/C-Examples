3cbdad681e9530199306456ff5e54e2047d8c04b
AndreyDodul <andrey.dodul@mail.ru>
2023-05-22 07:03:11 +0300

    Task to calculate average of the columns of 2-side array

---
+++ Task52d_AverColumn/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+        // Например, задан массив:
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+                // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+double averageColumn = 0;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    
+    Console.Write("Сумма элементов столбца " + ( j + 1 ) + " = " + sum + ";    ");
+    
+    averageColumn = sum / n;
+    Console.Write("Сред. арифм. столбца " + ( j + 1 ) + " = " + averageColumn + ";");
+    Console.WriteLine();
+}
+Console.ReadKey();
