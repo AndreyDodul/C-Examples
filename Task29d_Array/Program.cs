a11e0f248d1685ea40b54b7413631746edaae6e2
AndreyDodul <andrey.dodul@mail.ru>
2023-05-26 23:19:30 +0300

    Задать массив из 8-ми элементов и вывести на экран

---
+++ Task29d_Array/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов (от -10 до 10) и
+// выводит их на экран.
+// 1, 2, 5, 7, 9 -> [1, 2, 5, 7, 9]             6, 1, 3 -> [6, 1, 3]
+
+Console.WriteLine("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine()); // по задаче 8
+int[] array = new int[arraySize];
+
+for (int i = 0; i < array.Length; i++) // заполнение массива
+{
+    int randomNumber = new Random().Next(-10, 10); // генератор случайных чисел
+    array[i] = randomNumber; // помещение случайного числа в массив на место с индексом i
+
+    Console.Write(array[i] + ", "); // вывод в консоль результата - массива
+}
