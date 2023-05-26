2690ab66f26316169506edaec5976a1a552fa69c
AndreyDodul <andrey.dodul@mail.ru>
2023-05-26 22:54:02 +0300

    Программа вычисляет квадраты чисел от 1 до N

---
+++ Task33/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Программа принимает на вход число N и выдает квадраты от 1 до N");
+Console.WriteLine("Введите значение N: ");
+string str = Console.ReadLine();
+double n = Convert.ToInt32(str);
+
+// вариант 1
+// double x = 0;
+// double y = 0;
+
+// while (x < n) 
+// {
+//     y = Math.Pow(x, 2);
+//     Console.Write(y);
+//     Console.Write(" ");
+//     x++;
+// }
+
+// вариант 2
+Console.Write("Квадраты чисел от 1 до N = " + n + ": ");
+for(int i = 1; i <= n; Console.Write("  " + i * i + "  "), i++);
