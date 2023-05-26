07a054860c3495bdf5d43c72fa70cac9d19887d5
AndreyDodul <andrey.dodul@mail.ru>
2023-05-26 23:33:01 +0300

    Программа принимает на вход число N  и выдает таблицу кубов от 1 до N

---
+++ Task23d_cubes/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Программа принимает на вход число N и выдает таблицу кубов значений от 1 до N");
+Console.WriteLine("Введите значение N: ");
+string str = Console.ReadLine();
+double n = Convert.ToInt32(str);
+
+// вариант 1
+double x = 1;
+double y = 0;
+
+while (x < n) 
+{
+    y = Math.Pow(x, 3);
+    Console.Write(y);
+    Console.Write(" ");
+    x++;
+}
+
+// вариант 2
+// Console.Write("Значения кубов чисел от 1 до N = " + n + ": ");
+// for(int i = 1; i <= n; Console.Write("  " + i * i * i + "  "), i++);
