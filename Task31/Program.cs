7247cb9e7165f12ca99dd983203f6263fd2b1810
AndreyDodul <andrey.dodul@mail.ru>
2023-05-26 23:17:05 +0300

    Определение номера квадранта по координатам

---
+++ Task31/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Программа определяет номер квадранта по значениям X и Y");
+Console.WriteLine("Введите значение Х: ");
+string str1 = Console.ReadLine();
+int x = Convert.ToInt32(str1);
+Console.WriteLine("Введите значение Y: ");
+string str2 = Console.ReadLine();
+int y = Convert.ToInt32(str2);
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Значение 0 и 0 не вводи");
+}
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Значение " + x + " и " + y + " соответствуют квадранту 1.");
+}
+
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Значение " + x + " и " + y + " соответствуют квадранту 4.");
+}
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Значение " + x + " и " + y + " соответствуют квадранту 2.");
+}
+
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Значение " + x + " и " + y + " соответствуют квадранту 3.");
+}
