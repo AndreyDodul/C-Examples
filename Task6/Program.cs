878eb4dfd3e06e870ca285cb8501ed1262e85d66
AndreyDodul <andrey.dodul@mail.ru>
2023-05-30 06:41:55 +0300

    Программа проверки числа на чётность

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа определит, является ли число ЧЁТНЫМ");
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) {
+    Console.WriteLine("Число " + a + " - ЧЁТНОЕ");
+} else
+{ Console.WriteLine("Число " + a + " - Нечётное");}
+
+Console.WriteLine("Спасибо!");
