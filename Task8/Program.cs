5831ee808c04a20fe6bba1e51bf92919d83b27cd
AndreyDodul <andrey.dodul@mail.ru>
2023-05-30 06:40:25 +0300

    Программа принимает на вход число N и выдаёт числа от 1 до N

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа определит и выведет чётные числа от 1 до N");
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int a = 1; a < N; a++)
+    if (a % 2 == 0)
+        Console.Write(" " + a + " ");
+Console.WriteLine();
+
+Console.WriteLine("Спасибо!");
