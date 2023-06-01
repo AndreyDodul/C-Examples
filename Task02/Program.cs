1e54263817adb5c90f0c47804c531f371b452236
AndreyDodul <andrey.dodul@mail.ru>
2023-06-01 22:08:50 +0300

    Программа определяет наибольшее из 2-х чисел

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа определит НАИБОЛЬШЕЕ из 2-х целых чисел");
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (a > b) {
+    max = a;
+} else
+{ max = b; }
+
+Console.WriteLine("Наибольшее число: " + max);
+Console.WriteLine("Спасибо!");
